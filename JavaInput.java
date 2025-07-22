5a8057ca1ee2ff7b6155c063bc46860c4d5663f2
krishna-ammu05 <kirshnaejjigiri5@gmail.com>
2025-07-22 12:22:09 +0530

    Area of circle

--- JavaInput.java
+++ JavaInput.java
@@ -19,16 +19,30 @@
 // }
 
 //sum taking inputs from  Scanner
+// import java.util.*;
+// public class JavaInput{
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int a = sc.nextInt();
+//         int b = sc.nextInt();
+//         int sum = a + b;
+//         int product = a * b;
+//         System.out.println("Sum is :" + sum);
+//         System.out.println("Product is :" + product);
+//         sc.close();
+//     }
+// }
+
+
+//calculating area of a circle
 import java.util.*;
 public class JavaInput{
-    public static void main(String[] args) {
+    public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int sum = a + b;
-        int product = a * b;
-        System.out.println("Sum is :" + sum);
-        System.out.println("Product is :" + product);
+        float rad = sc.nextFloat();
+        float area = 3.14f * rad * rad ;
+        System.out.println(area);
         sc.close();
+        
     }
 }
