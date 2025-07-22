1fb8e214ef65faa2b96bae1f6be1d353d37b12f8
krishna-ammu05 <kirshnaejjigiri5@gmail.com>
2025-07-22 11:40:16 +0530

    JavaComments

---
+++ JavaComments.java
@@ -0,0 +1,15 @@
+public class JavaComments {
+    public static void main(String[] args) {
+        System.out.println("hello World!!");
+        //code to calculate sum
+        int a = 10;
+        int b =5;
+        int sum = a + b;
+        System.out.println(sum);
+        /*hi
+         * hello
+         * Java!!
+         */
+    }
+    
+}
