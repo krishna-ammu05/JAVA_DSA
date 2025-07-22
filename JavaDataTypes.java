b8095890efa2f3f812992ca2149cbfe96e785910
krishna-ammu05 <kirshnaejjigiri5@gmail.com>
2025-07-22 11:40:33 +0530

    JavaDataTypes

---
+++ JavaDataTypes.java
@@ -0,0 +1,12 @@
+public class JavaDataTypes{
+    public static void main(String args[]){
+        byte b=8;
+        System.out.println(b);
+        char ch='a';
+        System.out.println(ch);
+        boolean var=false;
+        float price=9;
+        int number = 25;
+        short n=240;
+    }
+}
