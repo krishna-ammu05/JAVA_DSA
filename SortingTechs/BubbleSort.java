363882e3bb228e5d9c4c644018656cddaa0b182b
krishna-ammu05 <krishnaveni5104@gmail.com>
2025-07-25 11:45:40 +0530

    Bubble_Sort

---
+++ SortingTechs/BubbleSort.java
@@ -0,0 +1,39 @@
+package SortingTechs;
+
+// import java.util.Arrays;
+import java.util.Scanner;
+
+public class BubbleSort {
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++)
+            for (int j = 0; j < n - i - 1; j++)
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+    }
+
+    public static void printArray(int[] arr) {
+        for (int val : arr) System.out.print(val + " ");
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter length of the Array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];//declaring array
+        System.out.println("Enter elements of the Array:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        
+        }
+         sc.close();
+        bubbleSort(arr);
+        printArray(arr);
+    }
+}
+
+
