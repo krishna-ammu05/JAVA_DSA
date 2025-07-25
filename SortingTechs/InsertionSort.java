88501d089069e6f320e712b605a0ff4f12e2dc14
krishna-ammu05 <krishnaveni5104@gmail.com>
2025-07-25 11:45:27 +0530

    Insertion_Sort

---
+++ SortingTechs/InsertionSort.java
@@ -0,0 +1,32 @@
+package SortingTechs;
+import java.util.Scanner;
+
+public class InsertionSort {
+    public static void insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int j = i;
+            while (j > 0 && arr[j] < arr[j - 1]) {
+                // Swapping directly inside the loop
+                int temp = arr[j];
+                arr[j] = arr[j - 1];
+                arr[j - 1] = temp;
+                j--;
+            }
+        }
+    }
+
+    public static void printArray(int[] arr) {
+        for (int val : arr) System.out.print(val + " ");
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) arr[i] = sc.nextInt();
+        sc.close();
+        insertionSort(arr);
+        printArray(arr);
+    }
+}
