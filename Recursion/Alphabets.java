518fbc2e4645df6ea9ab7a86708b99c843559d6e
krishna-ammu05 <krishnaveni5104@gmail.com>
2025-08-04 13:00:19 +0530

    Lexical Comparsion

---
+++ Recursion/Alphabets.java
@@ -0,0 +1,10 @@
+package Recursion;
+public class Alphabets {
+    public static void main(String[] args) {
+        if ( 'q' > 'a'){
+            System.out.println("q is greater than a");
+        } else {
+            System.out.println("q is not greater than a");
+        }
+    }
+}
