2e58a7e26921919573835747dfaacfc7de034cf7
krishna-ammu05 <krishnaveni5104@gmail.com>
2025-08-05 22:27:58 +0530

    update First occurrence

--- Recursion/FirstOccurence.java
+++ Recursion/FirstOccurence.java
@@ -20,3 +20,24 @@ public class FirstOccurence {
     }
     
 }
+// package Recursion;
+
+// public class FirstOccurrenceString {
+//     public static void main(String[] args) {
+//         String str = "recursion";
+//         char target = 'r';
+//         int index = findFirstOccurrence(str, target, 0);
+//         System.out.println("First occurrence of '" + target + "' is at index: " + index);
+//     }
+
+//     // Recursive function to find the first occurrence of a character in a string
+//     public static int findFirstOccurrence(String str, char target, int index) {
+//         if (index == str.length()) {
+//             return -1; // Target not found
+//         }
+//         if (str.charAt(index) == target) {
+//             return index; // Found the first occurrence
+//         }
+//         return findFirstOccurrence(str, target, index + 1); // Recur for next index
+//     }
+// }
