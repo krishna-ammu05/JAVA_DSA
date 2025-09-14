b9e733e696bd95b7ccb28a8de163da445f7533bc
krishna-ammu05 <krishnaveni5104@gmail.com>
2025-09-14 19:12:28 +0530

    CountNodes in a tree

---
+++ Trees/CountNodes.java
@@ -0,0 +1,41 @@
+package Trees;
+
+public class CountNodes {
+  
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+        Node(int data){
+            this.data = data;
+            this.left = null;
+            this.right= null;
+        }
+    }  
+    public static int countNodes(Node root){
+        if(root == null){
+            return 0;
+        }
+        int leftCount = countNodes(root.left);
+        int rightCount = countNodes(root.right);
+        return leftCount + rightCount +1;
+    }
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+        root.left.left.left = new Node(8);
+        root.left.left.right = new Node(9);
+        root.left.right.left = new Node(10);
+        root.left.right.left.left = new Node(11);
+
+        int Count = countNodes(root);
+        System.out.println("Height of the tree: " + Count);
+    }
+}
+
+
