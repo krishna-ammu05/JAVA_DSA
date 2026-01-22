dabbacc5d98b8a00b683356afe4a1ce83cb0b58d
krishna-ammu05 <krishnaveni5104@gmail.com>
2026-01-22 12:00:41 +0530

    left_view of Tree

---
+++ Trees/left_view.java
@@ -0,0 +1,49 @@
+package Trees;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+class TreeNode{
+    int data;
+    TreeNode left,right;
+    TreeNode(int data){
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+public class left_view {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+        root.right.right = new TreeNode(6);
+    
+        System.out.println("left view of tree:");
+        leftview_traversal(root);
+    }
+    public static void leftview_traversal(TreeNode root){
+        if(root == null){
+            return;
+        }
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        while(!q.isEmpty()){
+            int size = q.size();
+            for(int i =0;i<size;i++){
+                TreeNode curr = q.poll();
+                if(i ==0){
+                    System.out.println(curr.data);
+                }
+                if(curr.left != null){
+                    q.add(curr.left);
+                }
+                if(curr.right!= null){
+                    q.add(curr.right);
+                }
+            }
+        }
+    }
+}
