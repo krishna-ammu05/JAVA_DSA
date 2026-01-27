a35793cf619193800b32a01f9ee855bb4d4a280a
krishna-ammu05 <krishnaveni5104@gmail.com>
2026-01-27 16:29:51 +0530

    intersection_sortedarr

---
+++ SortingTechs/Intersection_sortedarr.java
@@ -0,0 +1,18 @@
+package SortingTechs;
+public class Intersection_sortedarr {
+    public static void main(String[] args) {
+        int[] arr1 = {1, 2, 2, 1};
+        int[] arr2 = {2, 2};
+        int[] result = intersect(arr1, arr2);
+        System.out.print("Intersection: ");
+        for (int num : result) {
+            System.out.print(num + " ");
+        }
+    }
+
+    public static int[] intersect(int[] nums1,int[] nums2){
+        
+    }
+
+    
+}
